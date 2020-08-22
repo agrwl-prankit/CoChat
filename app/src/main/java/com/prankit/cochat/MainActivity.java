fc845547d180d9561144c5ae94e0bb9bc307bf9a
Prankit Agarwal <55039847+agrwl-prankit@users.noreply.github.com>
2020-08-22 21:18:31 +0530

    Add ChatFragment, GroupsFragment and ContactsFragment

--- app/src/main/java/com/prankit/cochat/MainActivity.java
+++ app/src/main/java/com/prankit/cochat/MainActivity.java
@@ -3,10 +3,16 @@ package com.prankit.cochat;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabsAccessAdapter tabsAccessAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("CoChat");
+
+        viewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        tabsAccessAdapter = new TabsAccessAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabsAccessAdapter);
+        tabLayout = (TabLayout) findViewById(R.id.tabBar);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
