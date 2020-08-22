386f789f892a10689b6ffa36515e82b87b3a9d7d
Prankit Agarwal <55039847+agrwl-prankit@users.noreply.github.com>
2020-08-22 18:49:12 +0530

    Add ToolBar in activity_main.xml

--- app/src/main/java/com/prankit/cochat/MainActivity.java
+++ app/src/main/java/com/prankit/cochat/MainActivity.java
@@ -1,14 +1,20 @@
 package com.prankit.cochat;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("CoChat");
     }
 }
