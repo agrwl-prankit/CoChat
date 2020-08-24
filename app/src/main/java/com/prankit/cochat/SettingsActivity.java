0b5ffc92cc67419069c0892b4dce0416fdbb1c1d
Prankit Agarwal <55039847+agrwl-prankit@users.noreply.github.com>
2020-08-24 21:15:02 +0530

    add profile photo icon in activity_settings.xml

--- app/src/main/java/com/prankit/cochat/SettingsActivity.java
+++ app/src/main/java/com/prankit/cochat/SettingsActivity.java
@@ -3,12 +3,21 @@ package com.prankit.cochat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private  EditText userNameEditText, statusEditText;
+    private  Button updateSettingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        userNameEditText = (EditText) findViewById(R.id.setUserName);
+        statusEditText = (EditText) findViewById(R.id.setStatus);
+        updateSettingButton = (Button) findViewById(R.id.updateSettingButton);
     }
 }
